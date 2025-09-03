734e4be64b3342c0fdbb44d318e53045f64e0d10
Douglas Faria <hfsdouglas@gmail.com>
2025-09-03 19:54:20 -0300

    fix: corrige init e mantem logica no arquivo app

--- app.py
+++ app.py
@@ -1,3 +1,26 @@
-from app import create_app
+import os
+from flask import Flask
+from app.database.db import db
+
+def create_app():
+    app = Flask(__name__)
+
+    BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+    DB_PATH = os.path.join(BASE_DIR, "app", "database", "shortify.db")
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{DB_PATH}"
+
+    db.init_app(app)
+
+    from app.routes.api import api
+    api(app)
+
+    from app.routes.web import web
+    web(app)
+
+    return app
 
 app = create_app()
+
+if __name__ == "__main__":
+  app.run(debug=True)
